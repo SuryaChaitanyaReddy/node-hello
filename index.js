5fa19fa912fdc4c66474f664998f29538f4cb1b6
Palle Chaitanya Kumar Reddy <121803197+SuryaChaitanyaReddy@users.noreply.github.com>
2023-06-06 17:47:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'hello hai good morining chaitanya reddy have nice day!\n'
   res.end(msg);
 });
 
